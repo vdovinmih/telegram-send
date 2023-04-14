bd7a3f542a477d7b98cdea1574c4eb4dc68cc90b
Mikhail Vdovin <102321665+vdovinmih@users.noreply.github.com>
2023-04-14 05:06:38 +0300

    lock python-telegram-bot 13.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     python_requires=">=3.6",    # also update classifier below
     packages=["telegram_send"],
-    install_requires=["python-telegram-bot>=13.0", "colorama", "appdirs"],
+    install_requires=["python-telegram-bot==13.5", "colorama", "appdirs"],
     entry_points={"console_scripts": ["telegram-send=telegram_send.telegram_send:main"]},
 
     author="Rahiel Kasim",
